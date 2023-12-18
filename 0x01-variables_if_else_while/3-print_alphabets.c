bacac56f27c20eacfa931c9749c8e007d171f475
aymanhajji <hajjiayman03@gmail.com>
2023-12-18 22:13:15 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabetMin[] = "abcdefghijklmnopqrstuvwxyz";
+	char alphabetMax[] = "ABCDEFGHIJKLMNOPQESTUVWXYZ";
+	int i;
 
+	for (i = 0 ; i <= 25 ; i++)
+	{
+		putchar(alphabetMin[i]);
+		putchar(alphabetMax[i]);
+	}
+	putchar('\n');
+	return (0);
+}
