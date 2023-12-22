4a0acde2f1d54af2ae5db00c204b41f67351cff4
aymanhajji <hajjiayman03@gmail.com>
2023-12-22 20:15:34 +0100

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * main - tests the function if it prints negative or positive
- *
- * Return: 0
- */
-int main(void)
+void test_positive_or_negative()
 {
-	void test_positive_or_negative() {
-		printf("Test 1 - ");
-		positive_or_negative(10);
-		
-		printf("Test 2 - ");
-		positive_or_negative(0);
-		
-		printf("Test 3 - ");
-		positive_or_negative(-5);
-	}
+	printf("Test 1 - ");
+	positive_or_negative(10);
+
+	printf("Test 2 - ");
+	positive_or_negative(0);
+
+	printf("Test 3 - ");
+	positive_or_negative(-5);
+}
+
+int main()
+{
+	printf("Running Tests for positive_or_negative Function:\n");
+	test_positive_or_negative();
 	return (0);
 }
