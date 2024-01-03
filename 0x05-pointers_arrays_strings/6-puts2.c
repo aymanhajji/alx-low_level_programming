860b404db29fa191003ad229b1b7f893b50cfcf4
aymanhajji <hajjiayman03@gmail.com>
2024-01-04 00:09:13 +0100

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - it prints every other character of a string
+ * @str: pointer to the string we want to display
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
