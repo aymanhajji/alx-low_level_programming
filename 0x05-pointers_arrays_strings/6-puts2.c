d3f36cd72e26068810d1a97cce8c4e345c042a0d
aymanhajji <hajjiayman03@gmail.com>
2024-01-04 00:16:16 +0100

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,15 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	for (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i = i + 2;
+
+		if (str[i] == '\0')
+		{
+			break;
+		}
 	}
 	_putchar('\n');
 }
