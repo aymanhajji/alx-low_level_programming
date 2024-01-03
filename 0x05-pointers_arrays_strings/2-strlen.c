5017392a18014ca33ea834b412e2e3f1219db069
aymanhajji <hajjiayman03@gmail.com>
2024-01-03 20:40:33 +0100

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to a string that we want to count
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	char text = *s;
+	int i = 0;
+
+	while(text[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
