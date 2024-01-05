380e25303b2345c8411a7ca1c0433f5f8f4bf9c0
aymanhajji <hajjiayman03@gmail.com>
2024-01-06 00:12:27 +0100

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: pointer to a string
+ * @s2: pointer to a string
+ *
+ * Return: an integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int valeur = 1;
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		if (s1[i] != s2[j])
+		{
+			valeur = 0;
+		}
+		i++;
+		j++;
+	}
+	if (s1[i] == '\0')
+	{
+		j++;
+	}
+	else if (s2[j] == '\0')
+	{
+		i++;
+	}
+	if (i == j)
+	{
+		return (valeur);
+	}
+	else if (i < j)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (1);
+	}
+}
