4bfa3f349220473809403d99b20320baf48d21f1
aymanhajji <hajjiayman03@gmail.com>
2024-01-06 01:55:26 +0100

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,9 +10,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int c;
 
-	for (i = 0 ; i <= n ; i++)
+	for (i = 0 ; i <= n / 2 ; i++)
 	{
+		c = a[i];
 		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
 	}
 }
