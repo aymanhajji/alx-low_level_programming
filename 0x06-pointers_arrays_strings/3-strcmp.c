35fdb046dd8d558b95b98588454cae8ce5d11133
aymanhajji <hajjiayman03@gmail.com>
2024-01-06 01:17:18 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-
-	while (s1[i] != '\0' && s1[i] == s2[j])
+	while (*s1 != '\0' && *s1 == *s2)
 	{
-		i++;
-		j++;
+		s1++;
+		s2++;
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
