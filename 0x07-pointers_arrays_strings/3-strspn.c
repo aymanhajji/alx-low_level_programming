f9c35a7c3cb2267ccb3f18ef49edd3a2c54e4615
aymanhajji <hajjiayman03@gmail.com>
2024-01-10 01:14:16 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * int _strspn - gets the length of a prefix substring
+ * @s: pointer to a char
+ * @accept: pointer to a char
+ *
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
