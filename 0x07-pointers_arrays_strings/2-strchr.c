f444352c2486a2110d7f31102562214f634dbc46
aymanhajji <hajjiayman03@gmail.com>
2024-01-10 01:02:50 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strchr - locates a character in a string
+ * @s: pointer to a char
+ * @c: char
+ *
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	while (s != '\0')
+	{
+		if (s == c)
+		{
+			return (s);
+		}
+	}
+	return (NULL);
+}
