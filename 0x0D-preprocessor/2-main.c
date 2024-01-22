567f7d5da8358fbdc4eb3967c87f96390729e569
aymanhajji <hajjiayman03@gmail.com>
2024-01-22 22:04:08 +0100

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 /**
  * main - main file
  * @argc: int
@@ -8,7 +10,17 @@
  */
 int main(int argc, char *argv[])
 {
+	char *str;
+	int i;
+
 	(void)argc;
-	printf("%s\n", __FILE__);
+	(void)argv;
+	str = (char *)malloc(sizeof(__FILE__) * sizeof(char));
+	str = __FILE__;
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
