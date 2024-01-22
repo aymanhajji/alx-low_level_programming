d995f2e09171cd4e741d28546b6117e8c84889e8
aymanhajji <hajjiayman03@gmail.com>
2024-01-22 21:27:07 +0100

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main file
+ * @argc: int
+ * @argv: array of strings
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", __FILE__);
+	return (0);
+}
