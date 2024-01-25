13d3b84c298b9595cfb9ade87fd817892e9b91bc
aymanhajji <hajjiayman03@gmail.com>
2024-01-25 03:42:46 +0100

    What's my name

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given on each element of an array
+ * @array: pointer to an array of chars
+ * @size: unsigned int refers to the size of the array
+ * @action: pointer to a function
+ *
+ * Return: void
+ */
+void array_iterator(char *array, size_t size, void (*action)(int))
+{
+
+}
