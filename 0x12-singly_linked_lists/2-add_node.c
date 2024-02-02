3ad2cf0e44eff43c17457d8fc30f4ff2f642aeaa
aymanhajji <hajjiayman03@gmail.com>
2024-02-02 01:24:36 +0100

    Correcting a printing problem

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,12 +19,17 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <string.h>
+#include <stio.h>
+#include <stdlib.h>
+/**
+ * *add_node_end - adds a new node at the end of a list
+ * @head: pointer pointing to pointer to the head of list
+ * @str: pointer to char
+ *
+ * Return: The adress of new element
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
