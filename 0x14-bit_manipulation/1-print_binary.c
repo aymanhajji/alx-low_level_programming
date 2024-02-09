d0a5a14cd9807646917f897f4bcd7082a4767f52
aymanhajji <hajjiayman03@gmail.com>
2024-02-10 00:28:43 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,34 +8,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bits;
-	int i;
-	unsigned long int mask;
-	bool value;
+	int shift;
 
-	value = false;
-	bits = sizeof(n) * 8;
-	for (i = bits - 1 ; i >= 0 ; i--)
+	shift = sizeof(unsigned long int) * 8 - 1;
+	while (!(n >> shift))
 	{
-		mask = 1UL << i;
-		if (n == 0)
-		{
-			_putchar('0');
-			break;
-		}
-		else if (((n & mask) ? '1' : '0') == '0' && (value == true))
-		{
-			_putchar((n & mask) ? '1' : '0');
-		}
-		else if (((n & mask) ? '1' : '0') == '1' && (value == false))
-		{
-			value = true;
-			_putchar('1');
-		}
-		else if (((n & mask) ? '1' : '0') == '1' && (value == true))
-		{
-			_putchar('1');
-		}
+		shift--;
 	}
-	_putchar('\n');
+	for (; shift >= 0 ; shift--)
+	{
+		_putchar((n & (1UL << shift)) ? '1' : '0');
+	}
 }
