4a4692a537619ea9d7ef05fd1d53b0642de84f9b
aymanhajji <hajjiayman03@gmail.com>
2024-02-10 00:23:31 +0100

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,12 +11,31 @@ void print_binary(unsigned long int n)
 	int bits;
 	int i;
 	unsigned long int mask;
+	bool value;
 
+	value = false;
 	bits = sizeof(n) * 8;
 	for (i = bits - 1 ; i >= 0 ; i--)
 	{
 		mask = 1UL << i;
-		_putchar((n & mask) ? '1' : '0');
+		if (n == 0)
+		{
+			_putchar('0');
+			break;
+		}
+		else if (((n & mask) ? '1' : '0') == '0' && (value == true))
+		{
+			_putchar((n & mask) ? '1' : '0');
+		}
+		else if (((n & mask) ? '1' : '0') == '1' && (value == false))
+		{
+			value = true;
+			_putchar('1');
+		}
+		else if (((n & mask) ? '1' : '0') == '1' && (value == true))
+		{
+			_putchar('1');
+		}
 	}
 	_putchar('\n');
 }
