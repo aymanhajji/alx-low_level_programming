fc0225e6edae61218deb1ac5123f47026d3da11f
aymanhajji <hajjiayman03@gmail.com>
2024-02-16 18:27:18 +0100

    Speak gently, she can hear

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		{
 			len++;
 		}
-		bytes_written = write(fd, text_content, len + 1);
+		bytes_written = write(fd, text_content, len);
 		if (bytes_written == -1)
 		{
 			close(fd);
